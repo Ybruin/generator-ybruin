3451d75ca85ca5132ddf21648e1bbec60176887f
Huang Xiaoyan <hxysnail@gmail.com>
2016-07-26 14:39:32 +0800

    a generator to init ybruin project

---
+++ app/templates/ybruin-conf.js
@@ -0,0 +1,19 @@
+fis.config.set('projectConf',{
+    name:'<%= projectName %>',
+    terminal:'<%= projectAssets %>',
+    domainList:['http://res.cont.yy.com'],
+    compDomain:'http://res.cont1.yy.com',
+    outputPath:'../dist',
+    compressCss:true,
+    compressImg:true,
+    compressJs:true,
+    useSprite:true,
+    useHash:true
+})
+
+fis.config.set('projectConf.useSprite',{
+	to:'/images/sprites',
+    margin:2
+})
+
+fis.runConf();
